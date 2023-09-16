68acabf4002b880631af98fe7e718ae475f1f4a7
TigoF13 <tigo.ilhami13@gmail.com>
2023-09-16 13:26:49 +0700

    add dummy home ui

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selectedIndex = 0; // Indeks item yang dipilih
+
   @override
   Widget build(BuildContext context) {
     double fem = 1.0;
 
     return MaterialApp(
       home: Scaffold(
-        body: Stack(
+        body: IndexedStack(
+          index: _selectedIndex, // Indeks menentukan tampilan yang ditampilkan
           children: [
+            // Laman Home (indeks 0)
             Container(
               width: double.infinity,
               decoration: BoxDecoration(
@@ -32,11 +41,81 @@ class Home extends StatelessWidget {
                   ),
                 ],
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
+              child: Center(
+                child: Text(
+                  'Home Page',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+            ),
+
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(border: Border.all(color: Color(0xff000000)),
+                color: Color(0xffffffff),
+                gradient: LinearGradient(
+                  begin: Alignment(1, -0.98),
+                  end: Alignment(-1, 1),
+                  colors: <Color>[
+                    Color(0xffc63c8f),
+                    Color(0xff952b2b),
+                    Color(0xff5b1834),
+                  ],
+                  stops: <double>[0, 0.464, 1],
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0x3f000000),
+                    offset: Offset(0 * fem, 4 * fem),
+                    blurRadius: 2 * fem,
+                  ),
                 ],
+              ),
+              child: Center(
+                child: Text(
+                  'Pay Page',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+            ),
+
+            // Laman Account (indeks 2)
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(border: Border.all(color: Color(0xff000000)),
+                color: Color(0xffffffff),
+                gradient: LinearGradient(
+                  begin: Alignment(1, -0.98),
+                  end: Alignment(-1, 1),
+                  colors: <Color>[
+                    Color(0xffc63c8f),
+                    Color(0xff952b2b),
+                    Color(0xff5b1834),
+                  ],
+                  stops: <double>[0, 0.464, 1],
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0x3f000000),
+                    offset: Offset(0 * fem, 4 * fem),
+                    blurRadius: 2 * fem,
+                  ),
+                ],
+              ),
+              child: Center(
+                child: Text(
+                  'Account Page',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
               ),
             ),
           ],
@@ -66,6 +145,12 @@ class Home extends StatelessWidget {
           unselectedLabelStyle: TextStyle(
               fontWeight:
                   FontWeight.normal), // Gaya teks item yang tidak dipilih
+          currentIndex: _selectedIndex, // Mengikuti indeks yang dipilih
+          onTap: (int index) {
+            setState(() {
+              _selectedIndex = index; // Mengubah indeks yang dipilih
+            });
+          },
         ),
       ),
     );
