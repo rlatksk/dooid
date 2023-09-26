1e19e902bd415f473cca3e0aa4ce783f6f679443
ivander08 <ivanderseah08@gmail.com>
2023-09-26 22:49:59 +0700

    Made code much cleaner with self-made HomeCircleButtonIconText widget

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -13,13 +13,16 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: Column(
           children: [
             HomeTop(),
             HomeCard(),
-            SizedBox(height: 5),
-            HomeMainButtons()
+            SizedBox(height: 12),
+            HomeMainButtons(),
+            SizedBox(height: 30),
+            HomeQuickTransfer(),
           ],
         ),
       ),
@@ -85,30 +88,21 @@ class HomeTop extends StatelessWidget {
               ),
             ],
           ),
-          GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) =>
-                        Home()), // Change Home() to the Notifications Screen
-              );
-            },
-            child: Container(
-              width: 35,
-              height: 35,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(
-                  width: 1, 
-                  color: Colors.grey.shade400
-                ),
-              ),
-              child: Icon(
-                Icons.notifications_none_sharp,
-                color: Colors.grey.shade400,
-                size: 20,
-              ),
+          HomeCircleButtonIconText(
+            width: 35,
+            height: 35,
+            circleColor: Colors.white,
+            strokeColor: Colors.grey.shade400,
+            strokeSize: 1,
+            insideWidget: Icon(
+              Icons.notifications_none_sharp,
+              size: 20,
+              color: Colors.grey.shade400,
+            ),
+            navigateToGesture: GestureDetector(
+              onTap: () {
+                print('Notification Screen');
+              },
             ),
           ),
         ],
@@ -263,16 +257,11 @@ class HomeCircleButtonIconText extends StatelessWidget {
   final double height;
   final Color circleColor;
   final Color strokeColor;
-  final IconData? iconData;
-  final double? iconSize;
-  final ImageProvider? imageProvider;
-  final double? imageSize;
-  final String? buttonText;
-  final Color? textColor;
-  final double? textSize;
-  final FontWeight? fontWeight;
+  final double strokeSize;
+  final Widget? insideWidget;
+  final Widget? buttonTextWidget;
   final double? textCircleSpacing;
-  final Widget? navigateTo;
+  final GestureDetector? navigateToGesture;
 
   const HomeCircleButtonIconText({
     Key? key,
@@ -280,27 +269,19 @@ class HomeCircleButtonIconText extends StatelessWidget {
     required this.height,
     required this.circleColor,
     required this.strokeColor,
-    this.iconData,
-    this.iconSize,
-    this.imageProvider,
-    this.imageSize,
-    this.buttonText,
-    this.textColor,
-    this.textSize,
-    this.fontWeight,
+    required this.strokeSize,
+    this.insideWidget,
+    this.buttonTextWidget,
     this.textCircleSpacing,
-    this.navigateTo,
+    this.navigateToGesture,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        if (navigateTo != null) {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => navigateTo!),
-          );
+        if (navigateToGesture != null) {
+          navigateToGesture!.onTap?.call();
         }
       },
       child: Column(
@@ -313,43 +294,24 @@ class HomeCircleButtonIconText extends StatelessWidget {
               shape: BoxShape.circle,
               border: Border.all(
                 color: strokeColor,
-                width: 1.0,
+                width: strokeSize,
               ),
             ),
             child: Center(
-              child: iconData != null
-                  ? Icon(
-                      iconData,
-                      size: iconSize,
-                      color: Colors.white,
-                    )
-                  : imageProvider != null
-                      ? Image(
-                          image: imageProvider!,
-                          width: imageSize,
-                          height: imageSize,
-                        )
-                      : SizedBox.shrink(),
+              child: insideWidget ?? SizedBox.shrink(),
             ),
           ),
-          if (buttonText != null)
+          if (buttonTextWidget != null)
             SizedBox(
               height: textCircleSpacing ?? 8.0,
             ),
-          if (buttonText != null)
-            Text(
-              buttonText!,
-              style: GoogleFonts.montserrat(
-                fontSize: textSize ?? 14,
-                color: textColor,
-                fontWeight: fontWeight,
-              ),
-            ),
+          if (buttonTextWidget != null) buttonTextWidget!,
         ],
       ),
     );
   }
 }
+
 
 class HomeMainButtons extends StatelessWidget {
   const HomeMainButtons({
@@ -367,43 +329,254 @@ class HomeMainButtons extends StatelessWidget {
           HomeCircleButtonIconText(
             width: 85,
             height: 85,
-            circleColor: Colors.grey.shade300,
+            circleColor: Colors.grey.shade200,
             strokeColor: Colors.white,
-            imageProvider: AssetImage('assets/images/home/topup.png'),
-            imageSize: 40,
-            buttonText: 'Send',
-            textSize: 12,
+            strokeSize: 1,
+            insideWidget: Image.asset(
+              'assets/images/home/transfer.png', 
+              width: 40, 
+              height: 40),
+            buttonTextWidget: Text(
+              'Send',
+              style: GoogleFonts.montserrat(
+                fontSize: 12,
+                color: Color(0xFF131313),
+                fontWeight: FontWeight.normal,
+              )
+            ),
             textCircleSpacing: 5,
-            fontWeight: FontWeight.normal,
-            navigateTo: Home(),
+            navigateToGesture: GestureDetector(
+              onTap: () {
+                print('Transfer Screen');
+              },
+            ),
           ),
           HomeCircleButtonIconText(
             width: 85,
             height: 85,
-            circleColor: Colors.grey.shade300,
+            circleColor: Colors.grey.shade200,
             strokeColor: Colors.white,
-            imageProvider: AssetImage('assets/images/home/transfer.png'),
-            imageSize: 40,
-            buttonText: 'Top Up',
-            textSize: 12,
+            strokeSize: 1,
+            insideWidget: Image.asset(
+              'assets/images/home/topup.png', 
+              width: 40, 
+              height: 40),
+            buttonTextWidget: Text(
+              'Top Up',
+              style: GoogleFonts.montserrat(
+                fontSize: 12,
+                color: Color(0xFF131313),
+                fontWeight: FontWeight.normal,
+              )
+            ),
             textCircleSpacing: 5,
-            fontWeight: FontWeight.normal,
-            navigateTo: Home(),
+            navigateToGesture: GestureDetector(
+              onTap: () {
+                print('Top Up Screen');
+              },
+            ),
           ),
           HomeCircleButtonIconText(
             width: 85,
             height: 85,
-            circleColor: Colors.grey.shade300,
+            circleColor: Colors.grey.shade200,
             strokeColor: Colors.white,
-            imageProvider: AssetImage('assets/images/home/more.png'),
-            imageSize: 40,
-            buttonText: 'More',
-            textSize: 12,
+            strokeSize: 1,
+            insideWidget: Image.asset(
+              'assets/images/home/more.png', 
+              width: 40, 
+              height: 40),
+            buttonTextWidget: Text(
+              'More',
+              style: GoogleFonts.montserrat(
+                fontSize: 12,
+                color: Color(0xFF131313),
+                fontWeight: FontWeight.normal,
+              )
+            ),
             textCircleSpacing: 5,
-            fontWeight: FontWeight.normal,
-            navigateTo: Home(),
+            navigateToGesture: GestureDetector(
+              onTap: () {
+                print('More Screen');
+              },
+            ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class HomeQuickTransfer extends StatelessWidget {
+  const HomeQuickTransfer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 25),
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Quick Transfer',
+              style: GoogleFonts.montserrat(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF131313),
+              ),
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                HomeCircleButtonIconText(
+                  width: 65, 
+                  height: 65, 
+                  circleColor: Colors.white, 
+                  strokeColor: Colors.grey.shade200,
+                  strokeSize: 2,
+                  insideWidget: Text(
+                    'I',
+                    style: GoogleFonts.montserrat(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF131313),
+                    ),
+                  ),
+                  buttonTextWidget: Text(
+                    'Ivander',
+                    style: GoogleFonts.montserrat(
+                      fontSize: 10,
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                  textCircleSpacing: 4,
+                  navigateToGesture: GestureDetector(
+                    onTap: () {
+                      print('Transfer to Ivander Screen');
+                    },
+                  ),
+                ),
+                HomeCircleButtonIconText(
+                  width: 65, 
+                  height: 65, 
+                  circleColor: Colors.white, 
+                  strokeColor: Colors.grey.shade200,
+                  strokeSize: 2,
+                  insideWidget: Text(
+                    'RS',
+                    style: GoogleFonts.montserrat(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF131313),
+                    ),
+                  ),
+                  buttonTextWidget: Text(
+                    'Richard',
+                    style: GoogleFonts.montserrat(
+                      fontSize: 10,
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                  textCircleSpacing: 4,
+                  navigateToGesture: GestureDetector(
+                    onTap: () {
+                      print('Transfer to Richard Screen');
+                    },
+                  ),
+                ),
+                HomeCircleButtonIconText(
+                  width: 65, 
+                  height: 65, 
+                  circleColor: Colors.white, 
+                  strokeColor: Colors.grey.shade200,
+                  strokeSize: 2,
+                  insideWidget: Text(
+                    'RT',
+                    style: GoogleFonts.montserrat(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF131313),
+                    ),
+                  ),
+                  buttonTextWidget: Text(
+                    'Ruben',
+                    style: GoogleFonts.montserrat(
+                      fontSize: 10,
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                  textCircleSpacing: 4,
+                  navigateToGesture: GestureDetector(
+                    onTap: () {
+                      print('Transfer to Ruben Screen');
+                    },
+                  ),
+                ),
+                HomeCircleButtonIconText(
+                  width: 65, 
+                  height: 65, 
+                  circleColor: Colors.white, 
+                  strokeColor: Colors.grey.shade200,
+                  strokeSize: 2,
+                  insideWidget: Text(
+                    'TI',
+                    style: GoogleFonts.montserrat(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF131313),
+                    ),
+                  ),
+                  buttonTextWidget: Text(
+                    'Tigo',
+                    style: GoogleFonts.montserrat(
+                      fontSize: 10,
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                  textCircleSpacing: 4,
+                  navigateToGesture: GestureDetector(
+                    onTap: () {
+                      print('Transfer to Tigo Screen');
+                    },
+                  ),
+                ),
+                HomeCircleButtonIconText(
+                  width: 65, 
+                  height: 65, 
+                  circleColor: Colors.white, 
+                  strokeColor: Colors.grey.shade200,
+                  strokeSize: 2,
+                  insideWidget: Text(
+                    'JS',
+                    style: GoogleFonts.montserrat(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF131313),
+                    ),
+                  ),
+                  buttonTextWidget: Text(
+                    'Jason',
+                    style: GoogleFonts.montserrat(
+                      fontSize: 10,
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                  textCircleSpacing: 4,
+                  navigateToGesture: GestureDetector(
+                    onTap: () {
+                      print('Transfer to Jason Screen');
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
