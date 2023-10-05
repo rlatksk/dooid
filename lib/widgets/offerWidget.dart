d1a2764102aeb9281714ef70cf9cfdf482e66baa
Scorkinn <ruben.tobiachaiyadi@yahoo.com>
2023-10-05 19:17:43 +0700

    Added Timer on the carousel

--- lib/widgets/offerWidget.dart
+++ lib/widgets/offerWidget.dart
@@ -1,42 +1,49 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:slide_countdown/slide_countdown.dart';
 
 class TheCarouselSlider extends StatelessWidget {
-  final List<String> carouselImages = ['assets/promoImage/umm.jpg'];
-
+  final List<String> carouselImages = [
+    'assets/promoImage/umm.jpg',
+    'assets/promoImage/whatif.jpg'
+  ];
+  final List<DateTime> targetDates = [DateTime(2024), DateTime(2023)];
+  //The DateTime format, DateTime(2023, 12, 31, 23, 59, 59)
   @override
   Widget build(BuildContext context) {
-    return CarouselSlider(
+    return CarouselSlider.builder(
+      itemCount: carouselImages.length,
       options: CarouselOptions(
-          height: 200,
-          enlargeCenterPage: true,
-          autoPlay: true,
-          aspectRatio: 16 / 9,
-          autoPlayCurve: Curves.fastOutSlowIn,
-          enableInfiniteScroll: true,
-          autoPlayAnimationDuration: Duration(milliseconds: 800),
-          viewportFraction: 0.8,
-          // scrollDirection: Axis.vertical,
-          ),
-      items: carouselImages.map((assetPath) {
-        return Builder(
-          builder: (BuildContext context) {
-            return Container(
-              width: MediaQuery.of(context).size.width,
-              margin: EdgeInsets.symmetric(horizontal: 5.0),
-              decoration: BoxDecoration(
-                color: Colors.amber,
-              ),
-              child: Image.asset(
-                assetPath,
-                fit: BoxFit.cover,
-              ),
-            );
-          },
-        );
-      }).toList(),
+        height: 200.0,
+        enableInfiniteScroll: true,
+        enlargeCenterPage: true,
+        autoPlay: true,
+      ),
+      itemBuilder: (BuildContext context, int index, int realIndex) {
+        return _buildCarouselItem(context, index, realIndex);
+      },
     );
+  }
+
+  Widget _buildCarouselItem(BuildContext context, int index, int realIndex) {
+    String carouselImage = carouselImages[index];
+    DateTime targetDate = targetDates[index];
+    return Container(
+        margin: EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12.0),
+          image: DecorationImage(
+            image: AssetImage(carouselImage),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Align(
+            alignment: Alignment.topRight,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TimerCountdown(targetDate: targetDate),
+            )));
   }
 }
 
@@ -140,20 +147,95 @@ class CardSroll extends StatelessWidget {
         child: Column(
           children: [
             OfferCard(
-              title: '69% Discount', 
-              description: 'Pay with Dooid on this restaurant to get your 69% Off', 
-              cardImages: 'assets/promoImage/umm.jpg'),
+                title: '420% Discount',
+                description: 'NO NEED TO FKIN PAY',
+                cardImages: 'assets/promoImage/whatif.jpg'),
             OfferCard(
-              title: '69% Discount', 
-              description: 'Pay with Dooid on this restaurant to get your 69% Off', 
-              cardImages: 'assets/promoImage/umm.jpg'),
+                title: '69% Discount',
+                description:
+                    'Pay with Dooid on this restaurant to get your 69% Off',
+                cardImages: 'assets/promoImage/umm.jpg'),
             OfferCard(
-              title: '69% Discount', 
-              description: 'Pay with Dooid on this restaurant to get your 69% Off', 
-              cardImages: 'assets/promoImage/umm.jpg'),
+                title: '69% Discount',
+                description:
+                    'Pay with Dooid on this restaurant to get your 69% Off',
+                cardImages: 'assets/promoImage/umm.jpg'),
           ],
         ),
       ),
     );
+  }
+}
+
+class TimerCountdown extends StatefulWidget {
+  final DateTime targetDate;
+  const TimerCountdown({super.key, required this.targetDate});
+  @override
+  State<TimerCountdown> createState() => _TimerCountdownState();
+}
+
+class _TimerCountdownState extends State<TimerCountdown> {
+  late Duration _duration;
+  late bool _isTimerDone;
+
+  @override
+  void initState() {
+    super.initState();
+    _isTimerDone = false;
+    _calculateTimeRemaining();
+  }
+
+  void _calculateTimeRemaining() {
+    Duration remainingTime = widget.targetDate.difference(DateTime.now());
+    setState(() {
+      _duration = remainingTime;
+    });
+
+    if (_duration.inMilliseconds <= 0) {
+      setState(() {
+        _isTimerDone = true;
+      });
+    } else {
+      Future.delayed(Duration(seconds: 1), _calculateTimeRemaining);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _isTimerDone
+        ? Container(
+            decoration: BoxDecoration(
+              color: Color(0xFFFF5151),
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: Text(
+                "Expired",
+                style:
+                    GoogleFonts.montserrat(
+                      color: Color(0xFFBABABA), 
+                      fontSize: 13),
+              ),
+            ))
+        : SlideCountdown(
+            padding: EdgeInsets.all(8),
+            slideDirection: SlideDirection.up,
+            duration: _duration,
+            decoration: BoxDecoration(
+                color: Color(0xFFFF5151),
+                borderRadius: BorderRadius.circular(30)),
+            textStyle:
+                GoogleFonts.montserrat(color: Color(0xFFBABABA), fontSize: 13),
+            separatorStyle: GoogleFonts.montserrat(
+              color: Colors.black,
+              fontSize: 13,
+            ),
+            onDone: () {
+              setState(() {
+                _isTimerDone = true;
+              });
+            },
+          );
   }
 }
