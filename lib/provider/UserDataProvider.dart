add3db6a769a13cadd915a8bab8fdde8ce6a67e8
JasonFTI45 <jason.535220045@stu.untar.ac.id>
2023-10-05 10:59:50 +0700

    Temporary Profile Screen

---
+++ lib/provider/UserDataProvider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/foundation.dart';
+import 'UserDataModel.dart';
+
+class UserDataProvider extends ChangeNotifier {
+  UserDataModel? _userData;
+
+  UserDataModel? get userData => _userData;
+
+  void setUserData(String firstname, String pin) {
+    _userData = UserDataModel(firstname: firstname, pin: pin);
+    notifyListeners();
+  }
+
+  
+}
